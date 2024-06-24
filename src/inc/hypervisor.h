4b476a41eea00d290df03c76665b07555747044f
Zefinder <jakubiak@insa-toulouse.fr>
2024-06-24 15:36:04 +0200

    Add revoke measurement hypercall

--- src/inc/hypervisor.h
+++ src/inc/hypervisor.h
@@ -7,7 +7,7 @@
 #define HYPERCALL_BASE_VALUE 0x86000000 | 0x40000000
 
 /* Hypervisor actions (TODO Change name) */
-enum hypervisor_actions { HC_IPC = 1, HC_REQUEST_MEM_ACCESS = 2, HC_REVOKE_MEM_ACCESS = 3, HC_GET_CPU_ID = 4, HC_NOTIFY_CPU = 5, HC_EMPTY_CALL = 6, HC_REQUEST_MEM_ACCESS_TIMER = 7, HC_DISPLAY_STRING = 8, HC_MEASURE_IPI = 9 };
+enum hypervisor_actions { HC_IPC = 1, HC_REQUEST_MEM_ACCESS = 2, HC_REVOKE_MEM_ACCESS = 3, HC_GET_CPU_ID = 4, HC_NOTIFY_CPU = 5, HC_EMPTY_CALL = 6, HC_REQUEST_MEM_ACCESS_TIMER = 7, HC_DISPLAY_STRING = 8, HC_MEASURE_IPI = 9, HC_REVOKE_MEM_ACCESS_TIMER = 10 };
 
 /* Hypercall with the specified action and arguments */
 uint64_t hypercall(enum hypervisor_actions, int arg0, int arg1, int arg2);
